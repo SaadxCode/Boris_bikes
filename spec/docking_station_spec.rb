33f7ad1d64d60fab64c9c88ee5ce383084316986
Saad Khan <Saadkx@gmail.com>
2021-12-12 17:04:27 +0000

    incomplete chapter 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  
+  it { is_expected.to respond_to(:release_bike) }
+
+  it 'releases working bikes' do
+    expect(bike).to be_working
+  end
+
+  it { bike = Bike.new expect(subject.dock(bike)).to eq(bike) }
 end
